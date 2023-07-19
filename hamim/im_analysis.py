da52fb6bc9f62bcdf2daf37667ca7f5d1cafd97e
Schlermie <Scott.Bordelon@gmail.com>
2023-07-18 21:33:13 -0700

    Added im_analysis.py and send results to upload_success.html

---
+++ hamim/im_analysis.py
@@ -0,0 +1,15 @@
+#
+# Analyze a CSV file of frequencies for intermod conflicts.
+#
+
+def analyze(csv_file):
+    """ Analyze the list of frequencies in the CSV file for intermod """
+    """ conflicts. """
+    """ (NOT COMPLETED YET. For now, it just returns the CSV file contents) """
+    channel_list = csv2dict(csv_file)
+    return(channel_list)
+
+def csv2dict(csv_file):
+    """ Convert the CSV file to a dictionary of lists """
+    """ (NOT COMPLETED YET) """
+    return(csv_file)
--- hamim/views.py
+++ hamim/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .im_analysis import analyze
 from .forms import UploadFileForm
 
 # Create your views here.
@@ -9,12 +10,15 @@ def index(request):
         form = UploadFileForm(request.POST, request.FILES)
         print(form.errors.as_data())
         if form.is_valid():
-            uploaded_file = request.FILES['file']
-            file_content = uploaded_file.read().decode('utf-8')
-            # Do something with the file content
-            # For example, print it:
-            print(file_content)
-            return render(request, 'hamim/upload_success.html')
+            uploaded_file = request.FILES['file'].read().decode('utf-8')
+            # Analyze the frequencies in the CSV file for intermod and display
+            # the analysis results
+            analysis_results = analyze(uploaded_file)
+            print(analysis_results)
+            context = {
+                'analysis_results': analysis_results
+            }
+            return render(request, 'hamim/upload_success.html', context)
     else:
         form = UploadFileForm()
     return render(request, 'hamim/index.html', {'form': form})
