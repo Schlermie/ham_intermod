5627e83a87ac823cd04978911a6834ee1bd472e8
Schlermie <Scott.Bordelon@gmail.com>
2023-07-21 21:13:09 -0700

    Convert CSV string to a dictionary of lists.

--- hamim/im_analysis.py
+++ hamim/im_analysis.py
@@ -2,14 +2,37 @@
 # Analyze a CSV file of frequencies for intermod conflicts.
 #
 
-def analyze(csv_file):
-    """ Analyze the list of frequencies in the CSV file for intermod """
-    """ conflicts. """
-    """ (NOT COMPLETED YET. For now, it just returns the CSV file contents) """
-    channel_list = csv2dict(csv_file)
+def analyze(csv_string):
+    """
+    Analyze the list of frequencies in the CSV file for intermod
+    conflicts.
+    (NOT COMPLETED YET. For now, it just returns the CSV file contents)
+    """
+    channel_list = csv2dict(csv_string)
     return(channel_list)
 
-def csv2dict(csv_file):
-    """ Convert the CSV file to a dictionary of lists """
-    """ (NOT COMPLETED YET) """
-    return(csv_file)
+def csv2dict(csv_string):
+    """
+    Convert the CSV file to a dictionary of lists
+    """
+    data_dict = {}
+
+    # Convert the CSV string to a list of rows
+    csv_rows = csv_string.splitlines()
+
+    # Use the first row as column headers
+    headers = csv_rows[0].split(',')
+    num_columns = len(headers)
+
+    # Initialize lists for each column in the dictionary
+    for header in headers:
+        data_dict[header] = []
+
+    # Iterate over the remaining rows and populate the dictionary
+    for row in csv_rows[1:]:
+        row_data = row.split(',')
+        if len(row_data) == num_columns:
+            for i, header in enumerate(headers):
+                data_dict[header].append(row_data[i])
+
+    return data_dict
