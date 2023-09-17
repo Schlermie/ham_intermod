230b6051470afd0128f3dfc2151b0bc74f33edfa
Schlermie <Scott.Bordelon@gmail.com>
2023-09-16 20:18:08 -0700

    Check for 3rd order intermod

--- hamim/im_analysis.py
+++ hamim/im_analysis.py
@@ -40,11 +40,51 @@ def check_for_intermod(freqs):
     report_out = report_scores(report_out, freqs, aggressor_score, victim_score)
     return(report_out)
 
+def all_frequencies_kosher(f1, f2, f3):
+    """
+    Function to check if all three frequencies are valid for 3rd order intermod
+    analysis. Return True if valid. Return False if any of:
+      1. f1, f2, and f3 contain two opposite values
+      2. 
+      3. 
+    """
+    neg_f1 = f1 * -1
+    neg_f2 = f2 * -1
+    if (neg_f1 == f2) or (neg_f1 == f3) or (neg_f2 == f3):
+        return(False)
+    return(True)
+
 def check_for_3rd_order_intermod(report_out, freqs, agg_score, vic_score):
     """
     Scan the list of frequencies for any 3rd order intermod hits and report
     the results.
     """
+    report_out +="\n<h3><u>3rd Order Intermodulation</u></h3>"
+    analysis_freqs = append_negs_to_list_of_nums(freqs.copy()) # copy pointer
+    for i in range(0, len(freqs)): # Only loop through +ve nums
+        for j in range(i, len(analysis_freqs)):
+            for k in range(j, len(analysis_freqs)):
+                f1, f2, f3 = analysis_freqs[i], analysis_freqs[j], analysis_freqs[k]
+                if all_frequencies_kosher(f1, f2, f3):
+                    intermod = f1 + f2 + f3
+                    report_out += (f"{f1} + {f2} + {f3} = {intermod}")
+                    if intermod in freqs:
+                        report_out += (f" HIT\n")
+                        vic_score[freqs.index(intermod)] += 1
+                        agg_score[i] += 1
+                        if (abs(f2) != abs(f1)): # Don't double-count when scoring
+                            if j < len(freqs):
+                                agg_score[j] += 1
+                            else:
+                                agg_score[len(analysis_freqs)-j-1] += 1
+                        if (abs(f3) != abs(f1)) and (abs(f3) != abs (f2)): # Don't double-count when scoring
+                            if k < len(freqs):
+                                agg_score[k] += 1
+                            else:
+                                agg_score[len(analysis_freqs)-k-1] += 1
+                    else:
+                        report_out += (f"\n")
+    print(f"aggscore={agg_score} vicscore={vic_score}")
     return(report_out)
 
 def check_for_2nd_order_intermod(freqs, agg_score, vic_score):
@@ -64,10 +104,11 @@ def check_for_2nd_order_intermod(freqs, agg_score, vic_score):
                     report_out += (f" HIT\n")
                     vic_score[freqs.index(intermod)] += 1
                     agg_score[i] += 1
-                    if j < len(freqs):
-                        agg_score[j] += 1
-                    else:
-                        agg_score[len(analysis_freqs)-j-1] += 1
+                    if (abs(f2) != abs(f1)): # Don't double-count when scoring
+                        if j < len(freqs):
+                            agg_score[j] += 1
+                        else:
+                            agg_score[len(analysis_freqs)-j-1] += 1
                 else:
                     report_out += (f"\n")
     print(f"aggscore={agg_score} vicscore={vic_score}")
